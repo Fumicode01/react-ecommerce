5219772c3b5edde88c8304e2031a7cd51995cec6
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-07-27 13:54:09 +1000

    Create folder and file for firebase setting up and auth

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCkmIug4gFvRGBfjoywvAgZAnsA5AIc_78",
+    authDomain: "ec-app-5006b.firebaseapp.com",
+    projectId: "ec-app-5006b",
+    storageBucket: "ec-app-5006b.appspot.com",
+    messagingSenderId: "46681903804",
+    appId: "1:46681903804:web:1f7b4d30883426b18bb6b9",
+    measurementId: "G-PCCW6FC514"
+  };
+
