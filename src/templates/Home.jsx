2b2d66d8fec4ffe7ee302d17dd448dbc60cafabc
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-02 05:35:08 +1000

    Create SignOut function

--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { signOut } from '../redux/users/operations'
+import { getUserId, getUsername } from '../redux/users/selector'
 
 const Home = () => {
+    const dispatch = useDispatch()
+    const selector = useSelector(state => state)
+    const uid = getUserId(selector)
+    const username = getUsername(selector)
     return (
         <div>
-            Home
+            <h2>Home</h2>
+            <p>{uid}</p>
+            <p>{username}</p>
+            <button onClick={() => dispatch(signOut())}>SIGN OUT</button>
+
         </div>
     )
 }
