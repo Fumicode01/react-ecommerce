234efdabf2a4166d5da0d48e53e74d978c2f2dfb
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-01 13:04:53 +1000

    Create Auth Components

--- src/redux/users/selector.js
+++ src/redux/users/selector.js
@@ -2,12 +2,17 @@ import { createSelector } from "reselect";
 
 const usersSelector = (state) => state.users;
 
+export const getIsSignedIn = createSelector(
+    [usersSelector],
+    state => state.isSignedIn
+);
+
 export const getUserId = createSelector(
     [usersSelector],
     state => state.uid
 );
 
-export const getSignedIn = createSelector(
+export const getUsername = createSelector(
     [usersSelector],
-    state => state.isSignedIn
+    state => state.username
 );
