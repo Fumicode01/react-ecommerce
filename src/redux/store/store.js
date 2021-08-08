35f61b6fef62d76dec959db1f60a04eea440c9af
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-08 13:45:04 +1000

    Create products folder in redux
    create files for Products state manegement
    -operations.js
    -selectors.js
    -actions.js
    -reducers.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,8 @@
 import {createStore as reduxCreateStore, combineReducers, applyMiddleware} from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
 import {UsersReducer} from "../users/reducers";
+import {ProductsReducer} from "../products/reducers";
+
 import thunk from "redux-thunk";
 
 
@@ -8,7 +10,8 @@ export default function createStore(history){
     return reduxCreateStore(
         combineReducers({
             router:connectRouter(history),
-            users:UsersReducer
+            users:UsersReducer,
+            products:ProductsReducer,
         }),
         applyMiddleware(
             routerMiddleware(history),
