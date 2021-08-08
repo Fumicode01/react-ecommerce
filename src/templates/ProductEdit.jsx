fa01c118a6410332fd3fe40e91fce56fd2981258
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-08-08 13:36:54 +1000

    Create SelectBox Component in Product Edit page

--- src/templates/ProductEdit.jsx
+++ src/templates/ProductEdit.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useCallback} from 'react'
-import { TextInput } from '../components/Uikit'
+import { TextInput, SelectBox, PrimaryButton } from '../components/Uikit'
 
 const ProductEdit = () => {
 
@@ -21,23 +21,50 @@ const ProductEdit = () => {
     const InputPrice = useCallback((event) => {
         setPrice(event.target.value)
     }, [setPrice],
-)
+    )
+
+    const categories = [
+        {id:"tops", name:"Tops"},
+        {id:"shoes", name:"Shoes"},
+        {id:"cap", name:"Caps"},
+        {id:"bag", name:"Bags"},
+
+    ]
+    const genders = [
+        {id:"men", name:"Men"},
+        {id:"wemen", name:"Wemen"},
+        {id:"Unisex", name:"Unisex"},
+
+    ]
 
     return (
         <section>
-            <h2 className="u-text_-headline u-text-center">Resister Products</h2>
+            <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
+            <div className="module-spacer--medium" />
             <div className="c-section-container">
                 <TextInput 
-                    fullwidth={true} label={"ProductName"} multiline={false} 
+                    fullWidth={true} label={"ProductName"} multiline={false} 
                     required={true} onChange={InputName} rows={1} value={name} type={'text'}
                 />
                 <TextInput 
-                    fullwidth={true} label={"Description"} multiline={true} 
+                   fullWidth={true} label={"Description"} multiline={true} 
                     required={true} onChange={InputDescription} rows={5} value={description} type={'text'}
                 />
                 <TextInput 
-                    fullwidth={true} label={"Price"} multiline={false} 
+                    fullWidth={true} label={"Price"} multiline={false} 
                     required={true} onChange={InputPrice} rows={1} value={price} type={'number'}
+                />
+                <SelectBox 
+                    label={"Category"} required={true} options={categories} value={category} select={setCategory}
+                />
+                <SelectBox 
+                    label={"Gender"} required={true} options={genders} value={gender} select={setGender}
+                />
+            </div>
+            <div className="module-spacer--medium" />
+            <div className="center">
+                <PrimaryButton 
+                    label={"Submit"}
                 />
             </div>
         </section>
