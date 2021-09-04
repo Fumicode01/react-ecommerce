df5fd3087c54561c44fbb2bd018d6b42d3347d71
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-09-04 15:49:20 +1000

    Add Logo into Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { makeStyles} from '@material-ui/core/styles';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import { getIsSignedIn} from '../../redux/users/selector'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import logo from '../../assets/img/icons/logo.png'
+import { push } from 'connected-react-router'
 
 const useStyles = makeStyles({
     root:{
@@ -28,12 +29,13 @@ const useStyles = makeStyles({
 const Header = () => {
     const classes = useStyles();
     const selector = useSelector(state => state.state)
-
+    const dispatch = useDispatch();
+    const isSignedIn = getIsSignedIn(selector)
     return (
         <div className={classes.root}>
             <AppBar position="fixed" className={classes.menuBar}>
                 <Toolbar className={classes.toolBar}>
-
+                    <img src={logo} alt="Logo" width="128px" onClick={() => dispatch(push('/'))} />
                 </Toolbar>
             </AppBar>
         </div>
