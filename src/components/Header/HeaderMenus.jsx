f55d6dc27cf2149a56d46e733190b00e74385767
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-09-13 12:39:01 +1000

    create selector for cart

--- src/components/Header/HeaderMenus.jsx
+++ src/components/Header/HeaderMenus.jsx
@@ -4,13 +4,17 @@ import Badge from '@material-ui/core/Badge'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import MenuIcon from '@material-ui/icons/Menu'
+import {getProductsInCart} from '../../redux/users/selector'
+import { useSelector } from 'react-redux'
 
 
 const HeaderMenus = (props) => {
+    const selector = useSelector(state => state)
+    let productsInCart = getProductsInCart(selector)
     return (
         <>
             <IconButton>
-                <Badge badgeContent={3} color="secondary">
+                <Badge badgeContent={productsInCart.length} color="secondary">
                     <ShoppingCartIcon />
                 </Badge>
             </IconButton>
