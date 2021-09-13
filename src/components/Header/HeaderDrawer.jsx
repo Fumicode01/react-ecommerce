a46dab8b711f2788e442e8a36454cb20db8bcb7e
Fumicode01 <65881354+Fumicode01@users.noreply.github.com>
2021-09-13 10:44:00 +1000

    Create SideMenu Item List and func

--- src/components/Header/HeaderDrawer.jsx
+++ src/components/Header/HeaderDrawer.jsx
@@ -1,34 +1,59 @@
-import React from 'react'
-import { Divider, Drawer, List, ListItem, ListIconItem, ListItemText, IconButton } from '@material-ui/core'
-import { makeStyles } from '@material-ui/styles'
-import { Search, AddCircle, HistoryIcon, Person, ExitToApp } from '@material-ui/icons'
-import {textInput} from '../Uikit/index'
+import React, {useCallback, useState} from 'react'
+import { Divider, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/styles'
+import { Search, AddCircle, History, Person, ExitToApp } from '@material-ui/icons'
+import {TextInput} from '../Uikit/index'
+import { useDispatch } from 'react-redux'
+import { push } from 'connected-react-router'
+import {signOut} from '../../redux/users/operations'
 
 
-const useStyles = makeStyles((theme) => ({
-    drawer:{
-        [theme.breakpoint.up('sm')]:{
-            flexShrink:0,
-            width:256
+const useStyles = makeStyles((theme) =>
+    createStyles({
+        drawer: {
+            [theme.breakpoints.up('sm')]: {
+                width: 256,
+                flexShrink: 0,
+            }
         },
-        toolbar:theme.mixin.toolbar,
-        drawerPaper:{
-            width:256
+        // necessary for content to be below app bar
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: 256,
         },
-        searchField:{
-            alignItems:'center',
-            dispaly:'flex',
-            marginLeft:32
+        searchField: {
+            alignItems: 'center',
+            display: 'flex',
+            marginLeft: 32
         }
-    }
-}))
+    }),
+);
 
 
 const HeaderDrawer = (props) => {
     const classes = useStyles();
+    const dispatch = useDispatch()
     const {container} = props;
+    const [keyword, setKeyword] = useState("")
 
-    
+    const inputKeyword = useCallback((event) => {
+        setKeyword(event.target.value)
+        },[setKeyword])
+
+
+    const selectMenu = (event, path) => {
+        dispatch(push(path))
+        props.onClose(event)
+    }
+
+    const menus = [
+        {func:selectMenu, label: "Register Product", icon:<AddCircle />, id: "register", value:"/product/edit"},
+        {func:selectMenu, label: "OrderHistory", icon:<History />, id: "history", value:"/order/history"},
+        {func:selectMenu, label: "Profile", icon:<Person />, id: "profile", value:"/user/mypage"}
+    ]
+
+
+
     return (
         <nav className={classes.drawer}>
             <Drawer 
@@ -41,18 +66,38 @@ const HeaderDrawer = (props) => {
                 ModalPorps = {{keepMounted:true}}
                 >
                     <div>
-                        <div className={searchField}>
+                        <div className={classes.searchField}>
                             <TextInput 
                                 fullWidth={false}
-                                label={"Search Keywards"}
+                                label={"Search Keyward"}
                                 multiline={false}
-                                onChange={}
+                                onChange={inputKeyword}
                                 required={false}
                                 row={1}
-                                value={}
+                                value={keyword}
                                 type={"text"}
                                 />
+                                <IconButton>
+                                    <Search />
+                                </IconButton>
                         </div>
+                        <Divider />
+                        <List>
+                            {menus.map(menu => (
+                                <ListItem button key={menu.id} onClick={(e) => menu.func(e, menu.value)}>
+                                    <ListItemIcon>
+                                        {menu.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={menu.label} />
+                                </ListItem>
+                            ))}
+                            <ListItem button key="logout" onClick={() => dispatch(signOut())}>
+                                <ListItemIcon>
+                                    <ExitToApp />
+                                </ListItemIcon>
+                                <ListItemText primary={"Logout"} />
+                            </ListItem>
+                        </List>
                     </div>
 
             </Drawer>
